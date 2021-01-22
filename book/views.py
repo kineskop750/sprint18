82b882a4f9083c00d0913cae582855dd3fbfbd3c
Natalia Boichuk <boichuk.n.m@gmail.com>
2021-01-22 11:07:07 +0200

    edit, create and delete in the book

--- book/views.py
+++ book/views.py
@@ -5,35 +5,34 @@ from .forms import BookForm
 
 def get_all_books(request):
 	books = Book.objects.all()
-	context = {'books':books}
-	return render(request,'book/all_books.html',context)
+	context = {'books': books}
+	return render(request, 'book/all_books.html', context)
 
-def get_book(request,id):
+
+def get_book(request, id):
 	full = Book.get_by_id(id)
-	return render(request,'book/{id}.html',{'full':full})
+	return render(request, 'book/book.html', {'full': full})
 
-def new_book(request):
-	if request.method != "POST":
-		form = BookForm()
+
+def delete_book(request, id):
+	Book.delete_by_id(id)
+	return redirect('get_all_books')
+
+
+def book_form(request, id=0):
+	if request.method == 'GET':
+		if id == 0:
+			form = BookForm()
+		else:
+			book = Book.get_by_id(id)
+			form = BookForm(instance=book)
+		return render(request, 'book/book_form.html', {'form': form})
 	else:
-		form = BookForm(request.POST)
+		if id == 0:
+			form = BookForm(request.POST)
+		else:
+			book = Book.get_by_id(id)
+			form = BookForm(request.POST, instance=book)
 		if form.is_valid():
 			form.save()
 		return redirect('get_all_books')
-	return render(request,'book/new_book.html',{'form':form})
-
-def delete_book(request, id):
-	Book.objects.delete_by_id(id)
-	return redirect('get_all_books')
-
-def edit_book(request,id):
-	book = Book.objects.get(pk=id)
-	if request.method != 'POST':
-		form = BookForm()
-	else:
-		form = BookForm(instance=book,data=request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect('get_all_books')
-	return render(request,'book/edit_book.html',{'book':book,'form':form})
-	
